ebdb1fa43420bbf7319d6e1b6849bf8480ba91b3
soyel <contact@soyel.fr>
2012-11-01 01:16:47 +0100

    Creating the main model

---
+++ Model/App.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   App.cpp
+ * Author: soyel
+ *
+ * Created on November 1, 2012, 1:07 AM
+ */
+
+#include "App.h"
+
+namespace Model {
+    App::App() {
+    }
+
+    App::App(const App& orig) {
+    }
+
+    App::~App() {
+    }
+}
---
+++ Model/App.h
@@ -0,0 +1,23 @@
+/*
+ * File:   App.h
+ * Author: soyel
+ *
+ * Created on November 1, 2012, 1:07 AM
+ */
+
+#ifndef MODEL_APP_H
+#define	MODEL_APP_H
+
+namespace Model {
+    class App {
+        public:
+            App();
+            App(const App& orig);
+            virtual ~App();
+        private:
+
+    };
+}
+
+#endif	/* MODEL_APP_H */
+
